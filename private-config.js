b297f7323ccf15a03f81da3038ba684fdf5a6e35
xujl <xujl@weipaitang.com>
2020-06-30 17:56:37 +0800

    private-config

---
+++ private-config.js
@@ -0,0 +1,17 @@
+const Gitalk = require('gitalk');
+const md5 = require('md5');
+
+var gitalk = new Gitalk({
+    clientID: '548bd7170b285f9d5c22',
+    clientSecret: 'c70ea31537fb553d2d59622c5aca57fdf91b8fe5',
+    repo: 'xjl271314.github.io-my-blogs',
+    owner: 'xjl271314',
+    admin: ['xjl271314'],
+    id: md5(window.location.pathname), 
+    distractionFreeMode: false
+})
+
+module.exports = { 
+    gitalk 
+};
+
