8ed7561fb5da0d09ba64ea5192220987229b8dc4
许将龙 <xujl@weipaitang.com>
2022-02-08 15:58:02 +0800

    fix: comment-repo

--- private-config.js
+++ private-config.js
@@ -1,17 +1,16 @@
-const Gitalk = require('gitalk');
-const md5 = require('md5');
+const Gitalk = require("gitalk");
+const md5 = require("md5");
 
 var gitalk = new Gitalk({
-    clientID: '548bd7170b285f9d5c22',
-    clientSecret: 'c70ea31537fb553d2d59622c5aca57fdf91b8fe5',
-    repo: 'xjl271314.github.io/docs',
-    owner: 'xjl271314',
-    admin: ['xjl271314'],
-    id: md5(Date.now()), 
-    distractionFreeMode: false
-})
+  clientID: "548bd7170b285f9d5c22",
+  clientSecret: "c70ea31537fb553d2d59622c5aca57fdf91b8fe5",
+  repo: "https://github.com/xjl271314/docs",
+  owner: "xjl271314",
+  admin: ["xjl271314"],
+  id: md5(Date.now()),
+  distractionFreeMode: false,
+});
 
-module.exports = { 
-    gitalk 
+module.exports = {
+  gitalk,
 };
-
